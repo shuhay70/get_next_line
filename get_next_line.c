3520753e5f6fa2ca9d84af06e29294d13d227f3e
Horie Shuhei <hshuhei@c1r7s7.42tokyo.jp>
2025-12-08 13:50:26 +0900

    gnl関数の作成

--- get_next_line.c
+++ get_next_line.c
@@ -1,1 +1,43 @@
-char *get_next_line(int fd);
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hshuhei <hshuhei@student.42tokyo.jp>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/12/03 12:04:55 by hshuhei           #+#    #+#             */
+/*   Updated: 2025/12/07 17:50:33 by hshuhei          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+char	*get_next_line(int fd)
+{
+	static char	*stash;
+	char		*buf;
+	char		*newline_ptr;
+	int			bytes;
+	size_t		len;
+
+	if (fd < 0 || BUFFER_SIZE <= 0)
+		return (NULL);
+	buf = malloc(sizeof(char) * (BUFFER_SIZE + 1));
+	while (1)
+	{
+		if (stash && (newline_ptr = ft_strchr(stash, '\n')))
+			break ;
+		bytes = read(fd, buf, BUFFER_SIZE);
+		if (bytes <= 0)
+			break ;
+		buf[bytes] = '\0';
+		stash = gnl_strjoin(stash, buf);
+	}
+	free(buf);
+	if (!stash || *stash == '\0')
+	{
+		free(stash);
+		stash = NULL;
+		return (NULL);
+	}
+	newline_ptr = ft_strchr(stash, '\n');
+	len = newline_ptr ? (newline_ptr - stash + 1) : ft_strlen(stash);
+}
---
+++ get_next_line_utils.c
@@ -0,0 +1,70 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_utils.c                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hshuhei <hshuhei@student.42tokyo.jp>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/12/03 12:17:17 by hshuhei           #+#    #+#             */
+/*   Updated: 2025/12/05 15:31:24 by hshuhei          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdlib.h>
+
+static size_t	ft_strlen(const char *s)
+{
+	size_t	i;
+
+	i = 0;
+	while (s[i])
+		i++;
+	return (i);
+}
+
+char	*ft_substr(char const *s, unsigned int start, size_t len)
+{
+	size_t	s_len;
+	size_t	real_len;
+	size_t	i;
+	char	*str;
+
+	if (!s)
+		return (NULL);
+	s_len = ft_strlen(s);
+	if (start >= s_len)
+		real_len = 0;
+	else if (len < s_len - start)
+		real_len = len;
+	else
+		real_len = s_len - start;
+	str = malloc(sizeof(char) * (real_len + 1));
+	if (!str)
+		return (NULL);
+	i = 0;
+	while (i < real_len)
+	{
+		str[i] = s[start + i];
+		i++;
+	}
+	str[i] = '\0';
+	return (str);
+}
+
+char	*ft_strrchr(const char *s, int c)
+{
+	int	s_len;
+
+	s_len = ft_strlen(s);
+	while (s_len >= 0)
+	{
+		if (s[s_len] == (char)c)
+			return ((char *)&s[s_len]);
+		s_len--;
+	}
+	return (0);
+}
+
+char	*gnl_strjoin(char *s1, char *s2)
+{
+}
