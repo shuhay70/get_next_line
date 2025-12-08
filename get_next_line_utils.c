f412e67af6b3b5a40f7aefdfdecdd2a748e6b1bc
Horie Shuhei <hshuhei@c1r7s7.42tokyo.jp>
2025-12-08 17:55:47 +0900

    　アップデート

--- get_next_line_utils.c
+++ get_next_line_utils.c
@@ -6,7 +6,7 @@
 /*   By: hshuhei <hshuhei@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/03 12:17:17 by hshuhei           #+#    #+#             */
-/*   Updated: 2025/12/05 15:31:24 by hshuhei          ###   ########.fr       */
+/*   Updated: 2025/12/08 17:31:33 by hshuhei          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,34 +22,34 @@ static size_t	ft_strlen(const char *s)
 	return (i);
 }
 
-char	*ft_substr(char const *s, unsigned int start, size_t len)
-{
-	size_t	s_len;
-	size_t	real_len;
-	size_t	i;
-	char	*str;
+//char	*ft_substr(char const *s, unsigned int start, size_t len)
+//{
+//	size_t	s_len;
+//	size_t	real_len;
+//	size_t	i;
+//	char	*str;
 
-	if (!s)
-		return (NULL);
-	s_len = ft_strlen(s);
-	if (start >= s_len)
-		real_len = 0;
-	else if (len < s_len - start)
-		real_len = len;
-	else
-		real_len = s_len - start;
-	str = malloc(sizeof(char) * (real_len + 1));
-	if (!str)
-		return (NULL);
-	i = 0;
-	while (i < real_len)
-	{
-		str[i] = s[start + i];
-		i++;
-	}
-	str[i] = '\0';
-	return (str);
-}
+//	if (!s)
+//		return (NULL);
+//	s_len = ft_strlen(s);
+//	if (start >= s_len)
+//		real_len = 0;
+//	else if (len < s_len - start)
+//		real_len = len;
+//	else
+//		real_len = s_len - start;
+//	str = malloc(sizeof(char) * (real_len + 1));
+//	if (!str)
+//		return (NULL);
+//	i = 0;
+//	while (i < real_len)
+//	{
+//		str[i] = s[start + i];
+//		i++;
+//	}
+//	str[i] = '\0';
+//	return (str);
+//}
 
 char	*ft_strrchr(const char *s, int c)
 {
@@ -63,6 +63,46 @@ char	*ft_strrchr(const char *s, int c)
 		s_len--;
 	}
 	return (0);
+}
+
+unsigned int	ft_strlcpy(char *dst, const char *src, unsigned int size)
+{
+	unsigned int	i;
+	unsigned int	strlen;
+
+	i = 0;
+	strlen = 0;
+	while (src[strlen])
+		strlen++;
+	if (size == 0)
+		return (strlen);
+	while (i < (size - 1) && src[i])
+	{
+		dst[i] = src[i];
+		i++;
+	}
+	dst[i] = '\0';
+	return (strlen);
+}
+
+char	*ft_strdup(const char *s)
+{
+	int		s_len;
+	char	*ptr;
+	int		i;
+
+	i = 0;
+	s_len = ft_strlen(s);
+	ptr = malloc(s_len + 1);
+	if (ptr == NULL)
+		return (NULL);
+	while (i < s_len)
+	{
+		ptr[i] = s[i];
+		i++;
+	}
+	ptr[i] = '\0';
+	return (ptr);
 }
 
 char	*gnl_strjoin(char *s1, char *s2)
